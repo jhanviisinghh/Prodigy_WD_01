9498662841d79fb6401157e69f43c8728ebe7268
jhanviiii <136688010+jhanviisinghh@users.noreply.github.com>
2024-05-26 14:27:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor =  '#4682B4' ;
+    if (window.scrollY > 5) {
+        navbar.style.backgroundColor = '#4682B4';
     } else {
         navbar.style.backgroundColor = '#000080';
     }
